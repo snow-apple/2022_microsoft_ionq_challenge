3d1eb2d2ee5690a7b717f510298d7db9a7168f37
snow-apple <sjha40@stuy.edu>
2022-01-30 10:45:09 -0500

    Updated Levels, added gates, updated readme

--- README.md
+++ README.md
@@ -1,32 +1,44 @@
-# THE QUANTUM QUIZ
+# QUANTUM JEOPARDY
 Employ Quantum states to increase your chances to WIN!
 
 ## TEAM
-Shwetlana Jha , Skai  , Abha Naik
+Shwetlana Jha , Skai Nzeuton , Abha Naik
 
 ## DATE
 30 Jan 2022
 
 ## ABSTRACT
 We can hear people cheering aloud as the high school quiz team contest for a National level trophy. As a member of the team you wish to win as many points as possible. After the cut-throat rounds with rivals here comes the round where your Quantum Computing Knowledge comes to your aid. In this round you are given 4 topics,one topic is the field of your expertise.
-You badly wish that your team gets this topic,however unlike a Classical Quiz the possibility of getting the topic is not all random. These topics are in a superposition in a 2 qubit system.Each topic represent a possible outcome of measuring the superposition -AND THAT OUTCOME is the topic for your team. You are super excited as you are told that by applying some gates you will be able to manipulate the current state of the system so as to maximise the probability of finding your target topic.
+You badly wish that your team gets this topic,however unlike a Classical Quiz the possibility of getting the topic is not all random. These topics are in a superposition in a 2 qubit system. Each topic represent a possible outcome of measuring the superposition -AND THAT OUTCOME is the topic for your team. You are super excited as you are told that by applying some gates you will be able to manipulate the initial quantum state of the system so as to maximise the probability of finding your target topic. This initial quantum state can get increasingly complex as you increase the level, making the game more difficult. 
 
 ## HOW TO PLAY
 1. Participants begin with choosing the number of rounds.(In the case of a Quiz this number would be decided by the organisers)
 2. The list of Topics will be displayed with the corresponding state.
-3. The initial state of the circuit will be displayed. It will be an arbitary state and participants can use upto 3 gates to manipulate this initial state inorder to   maximise the probability of finding their desired topic.
-4. The state of the modified circuit will be displayed and measurement executed.
-5. Once the job is executed you will get your topic! (This is the time to rejoy if you had applied the right gates as you get the topic of your expertise, if you failed to apply the right gates...Good Luck with the other topic.You can also be clever and create a superposition between 2 topics : one of your expertise and other your second preferance.)
+3. The initial state of the circuit will be displayed. It will be an arbitary state depending on the level and participants can use upto 3 gates to manipulate this initial state in order to maximise the probability of finding their desired topic.
+4. The state of the modified circuit will be displayed and measurement executed on either a simulator or hardware.
+5. Once the job is executed you will get your topic! (This is the time to rejoice if you had applied the right gates as you get the topic of your expertise. However, if you failed to apply the right gates...Good Luck with the other topic. You can also be clever and create a superposition between 2 topics : one of your expertise and another of your second preferance.)
 6. Once you get a topic you will have to answer a question that will be randomly selected related to the topic.
 7. A correct answer will add a point to your team's scoreboard and a wrong one will result in a negative marking.
 8. The game will loop depending on the number of rounds.
+9. At the end, your total score will be displayed. 
 
+![Visualization]()
 ## HOW HIGH IS THE PROBABILITY OF FINDING QUANTUMNESS IN THIS GAME?
-While in a classical quiz is absolutely random or the players can manually select a topic, adding the properties of gate operations not only adds a superposition of predictability and randomness to the game but also makes the players develop a need to learn about gate operations and probability amplitude and creates curiosity in the minds of the viewers about this wierd thing called "superposition".Here the topic until selected is in a superposition of all topics.
+While a classical quiz game is absolutely random or requiring players to  manually select a topic, adding the properties of quantum gate operations not only adds a superposition of predictability and randomness to the game, but also makes the players develop a need to learn about gate operations and probability amplitude and creates curiosity in the minds of the viewers about this weird thing called "superposition". Here the topic until selected is in a complex superposition of all topics.
 
 ## IMPLEMENTATION
+We created a `QuantumJeopardy` class, which can be used to run the game described above. Below, we have pasted some example runs.
 
+#### Setup
+<img src="img/setup.png" style="width: 100%">
 
+#### Level 0
+<img src="img/Level0.png" style="width: 100%">
 
+#### Level 1
+<img src="img/Level1.png" style="width: 100%">
+
+#### Level 2
+<img src="img/Level2.png" style="width: 100%">
 
 
--- quantum_jeopardy/quantum.py
+++ quantum_jeopardy/quantum.py
@@ -9,6 +9,7 @@ import math
 class QuantumJeopardy:
     def __init__(self):
         self.backend = None
+        self.setup_game()
     
     def setup_backend(self, resource_id , backend_type = "ionq.simulator"):
         provider = AzureQuantumProvider (
@@ -18,10 +19,143 @@ class QuantumJeopardy:
         print([backend.name() for backend in provider.backends()])
         self.backend = provider.get_backend(backend_type)
 
+    def setup_game(self):
+        self.points = 0
 
+        #Get number of rounds
+        print("How many rounds? ")
+        self.rounds = int(input())
+        print("") #Spacing
+
+        #List of quiz topics
+        self.topicsList = ["Quantum", "Astronomy", "Math", "Geography"]
+
+        #Print the topics
+        print("The topics are: ")
+        for topic in self.topicsList:
+            print(topic)
+
+        #Lists that have the questions and answers for each topic
+        self.quantumQs = ["Bell states are a example of_________", "The CNOT gate is a ____ Qubit Gate"]
+        self.quantumAs = ["entangled state", "2"]
+
+        self.astronomyQs = ["A supermassive black hole that exists at the centre of the Milky Way Galaxy is _________", "The closest galaxy to the Milky Way is ___________"]
+        self.astronomyAs = ["sagittarius a", "andromeda"]
+
+        self.mathQs = ["Hilbert space is ____ Dimensional", "Complex numbers are __________ Dimensional"]
+        self.mathAs = ["infinite", "2"]
+
+        self.geographyQs = ["South Pole is situated in which continent?", "The great Barrier reef is situated in ________"]
+        self.geographyAs = ["antartica", "australia"]
+        
+    def run(self, level = 0):
+        #Start the game
+        for i in range(self.rounds):
+            print("Round " + str(i + 1))
+            print("="*20)
+
+            print("Here are the topics: " + str(self.topicsList))
+            topic_number = self.play_round(level = level)
+
+            #quantum pick topic
+            topic = self.topicsList[topic_number]
+            print("The topic is " + topic + "!")
+            print("Enter your answers in lowercase letters only. For numbers, use integers only.")
+            print("") #Spacing
+
+            if(topic == "Quantum"):
+                #Randomly pick question
+                index = random.randint(0, len(self.quantumQs) - 1)
+                question = self.quantumQs[index]
+                realAns = self.quantumAs[index]
+
+                #Ask question
+                print(question)
+                playerAns = input()
+
+                #Check answer
+                if(playerAns == realAns):
+                    print("Correct!")
+                    self.points += 1
+                    print("Points: " + str(self.points))
+                else:
+                    print("Incorrect! The answer is " + realAns + ".")
+                    self.points -= 1
+                    print("Points: " + str(self.points))
+
+            elif(topic == "Astronomy"):
+                #Randomly pick question
+                index = random.randint(0, len(self.astronomyQs) - 1)
+                question = self.astronomyQs[index]
+                realAns = self.astronomyAs[index]
+
+                #Ask question
+                print(question)
+                playerAns = input()
+
+                #Check answer
+                if(playerAns == realAns):
+                    print("Correct!")
+                    self.points += 1
+                    print("Points: " + str(self.points))
+                else:
+                    print("Incorrect! The answer is " + realAns + ".")
+                    self.points -= 1
+                    print("Points: " + str(self.points))
+
+            elif(topic == "Math"):
+                #Randomly pick question
+                index = random.randint(0, len(self.mathQs) - 1)
+                question = self.mathQs[index]
+                realAns = self.mathAs[index]
+
+                #Ask question
+                print(question)
+                playerAns = input()
+
+                #Check answer
+                if(playerAns == realAns):
+                    print("Correct!")
+                    self.points += 1
+                    print("Points: " + str(self.points))
+                else:
+                    print("Incorrect! The answer is " + realAns + ".")
+                    self.points -= 1
+                    print("Points: " + str(self.points))
+
+            else:
+                #Randomly pick question
+                index = random.randint(0, len(self.geographyQs) - 1)
+                question = self.geographyQs[index]
+                realAns = self.geographyAs[index]
+
+                #Ask question
+                print(question)
+                playerAns = input()
+
+                #Check answer
+                if(playerAns == realAns):
+                    print("Correct!")
+                    self.points += 1
+                    print("Points: " + str(self.points))
+                else:
+                    print("Incorrect! The answer is " + realAns + ".")
+                    self.points -= 1
+                    print("Points: " + str(self.points))
+
+            print("") #Spacing for next round
+        print("="*20)
+        print("GAME OVER! Your final score is: " + str(self.points))
+
+
+        
+        
     def apply_gates(self, gates, qc):
         for gate in gates:
-            getattr(qc,gate[0])(gate[1])
+            if len(gate) < 3:
+                getattr(qc,gate[0])(gate[1])
+            else: 
+                getattr(qc,gate[0])(gate[1],gate[2])
         return qc
 
 
@@ -30,10 +164,14 @@ class QuantumJeopardy:
         gate_set = []
         for i in range(3):
             gate_type = input("What gate would you like to use? (Type None if no gate): ")
-            if gate_type in ["x", "y", "z", "t", "None"]:
-                if gate_type != "None":
+            if gate_type in ["x", "y", "z", "t", "cx", "h", "None"]:
+                if gate_type != "None" and gate_type != "cx" :
                     qubit_type = int(input("Which qubit would you like to apply the gate to?: "))
                     gate_set.append((gate_type, qubit_type))
+                elif gate_type == "cx":
+                    qubit_type1 = int(input("Which qubit is the control?: "))
+                    qubit_type2 = int(input("Which qubit is the target?: "))
+                    gate_set.append((gate_type, qubit_type1, qubit_type2))
                 else:
                     print("skipped code")
             else:
@@ -41,7 +179,7 @@ class QuantumJeopardy:
 
         return gate_set
 
-    def play_round(self, make_random = True):
+    def play_round(self, level = 0):
         """
         1. It initializes the circuit in a random state
         2. It calls get_input() to let the user choose which gates they want to apply to the circuit. The user's
@@ -54,21 +192,35 @@ class QuantumJeopardy:
         
         if self.backend is None:
             raise ValueError("please setup backend first ")
-            
+        
+        print("These topics correspond to the following quantum states: " + str(["|00>", "|01>", "|10>", "|11>"]))
         # circuit initialization (We can set the rotation angles randomly) 
         circuit = QuantumCircuit(2, 2)
         circuit.h(0)
         circuit.cx(0,1) #this creates a bell state |00> + |11> . The qubits are maximaly entangled.
 
         # adding random rotations
-        if make_random:
+        if level == 0: 
+            pass
+        elif level == 1:
+            if random.random() > 0.5:
+                circuit.x(0)
+            if random.random() > 0.5:
+                circuit.z(0)
+            if random.random() > 0.5:
+                circuit.x(1)
+            if random.random() > 0.5:
+                circuit.z(1)
+        elif level == 2:
             rotations = [random.random()*2*math.pi for i in range(4)]
             circuit.rx(rotations[0],0)
             circuit.rz(rotations[1],0)
             circuit.rx(rotations[2],1)
             circuit.rz(rotations[3],1)
+            
 
         print(circuit)
+        print("A random initial quantum state has been prepared. You can add up to 3 gates to maximize the probability of measuring the quantum state corresponding to the topic of your choice. You are allowed to use the x, y, z, cx, h, and t gates.\n")
         gate_set = self.get_input()           
         circuit = self.apply_gates(gate_set, circuit)
         circuit.measure([0, 1], [0, 1])
@@ -100,3 +252,6 @@ class QuantumJeopardy:
         job_monitor(job)
         result = job.result()
         return result
+    
+    def reset(self):
+        self.setup_game()
